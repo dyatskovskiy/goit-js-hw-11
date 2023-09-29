2ec686304125037b897da1dc9ed8cbe4fb2fd782
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-29 16:43:16 +0300

    submit handler

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchPhotoByQuery } from './fetch-api.js';
+import axios from 'axios';
+
+const searchForm = document.querySelector('#search-form');
+const queryInput = document.querySelector('input');
+const searchBtn = document.querySelector('.search-btn');
+
+searchForm.addEventListener('click', onSubmitClick);
+
+function onSubmitClick(event) {
+  event.preventDefault();
+}
+
+// <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>;
